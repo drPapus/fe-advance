3bb57f4d2a7b0865709f1503784636bea313c6e5
Klimov Anton <antonklimovv@gmail.com>
2017-11-28 18:02:24 +0200

    sknd commit

--- module-1/task/script.js
+++ module-1/task/script.js
@@ -1,27 +1,38 @@
 const firstRow = 'qwertyuiop[]';
-const secondRow = 'asdfghjkl;';
+const secondRow = "asdfghjkl;'";
 const thirdRow = 'zxcvbnm,./';
 
 console.log(firstRow.length); //12
-console.log(secondRow.length); //10
+console.log(secondRow.length); //11
 console.log(thirdRow.length); //10
 
 const firstRowNum = 12;
-const secondRowNum = 10;
+const secondRowNum = 11;
 const thirdRowNum = 10;
 
-console.log(secondRowNum);
 
-console.log(firstRow.charAt(0));
-console.log(firstRow.charAt(11));
-console.log(secondRow.charAt(0));
-console.log(secondRow.charAt(9));
-console.log(thirdRow.charAt(0));
-console.log(thirdRow.charAt(9));
+var frstSymbol1str = firstRow.charAt(0);
+var lstSmblFirstRow = firstRow.charAt(11);
+var frstSymbol2str = secondRow.charAt(0);
+var lstSmblSecondRow = secondRow.charAt(10);
+var frstSymbol3str = thirdRow.charAt(0);
+var lstSmblThrdRow = thirdRow.charAt(9);
 
-var brackets = firstRow.indexOf('[]');
+var brackets = firstRow.indexOf('[');
+var brackets2 = firstRow.indexOf(']');
 
 console.log(brackets);
+console.log(brackets2);
+
+document.write(`Первый символ строки ${firstRow} это ${frstSymbol1str} <br/>
+Последний это знак ${lstSmblFirstRow} <br/>
+Первый символ строки ${secondRow} это ${frstSymbol2str} <br/>
+Последний это знак ${lstSmblSecondRow} <br/>
+Первый символ строки ${thirdRow} это ${frstSymbol3str} <br/>
+Последний это знак ${lstSmblThrdRow} <br/>
+<br/>
+Поиск по строке ${firstRow} показал что, знаки [] занимают ${brackets} и ${brackets2} -ое место. 
+` );
 
 
 
