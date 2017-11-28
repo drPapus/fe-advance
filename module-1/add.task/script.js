da5c071be0067363ef3dc8359358ce7d57c3cf0b
Klimov Anton <antonklimovv@gmail.com>
2017-11-28 16:49:37 +0200

    first commit

---
+++ module-1/add.task/script.js
@@ -0,0 +1,37 @@
+const a = 6;
+const b = 10;
+
+let result;
+
+result = a + b;
+console.log(result); //16
+
+result = b - (a + b);
+console.log(result); // -6
+
+result = a * b;
+console.log(result); // 60
+
+result = (a / b);
+console.log(result); // 0.6
+
+result = (b % a);
+console.log(result); // 4
+
+//Доп. задания пункт№1
+
+const day = '02';
+const month = '10';
+const year = 2017;
+
+
+
+const date = day + "\\" + month + "\\" + year;
+
+console.log(date); // "02/10/2017"
+
+
+
+
+
+
---
+++ module-1/task/script.js
@@ -0,0 +1,31 @@
+const firstRow = 'qwertyuiop[]';
+const secondRow = 'asdfghjkl;';
+const thirdRow = 'zxcvbnm,./';
+
+console.log(firstRow.length); //12
+console.log(secondRow.length); //10
+console.log(thirdRow.length); //10
+
+const firstRowNum = 12;
+const secondRowNum = 10;
+const thirdRowNum = 10;
+
+console.log(secondRowNum);
+
+console.log(firstRow.charAt(0));
+console.log(firstRow.charAt(11));
+console.log(secondRow.charAt(0));
+console.log(secondRow.charAt(9));
+console.log(thirdRow.charAt(0));
+console.log(thirdRow.charAt(9));
+
+var brackets = firstRow.indexOf('[]');
+
+console.log(brackets);
+
+
+
+
+
+
+
