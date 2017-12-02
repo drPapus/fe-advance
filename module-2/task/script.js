75da207c072748dadaedf445610a1614e2584fcd
Klimov Anton <antonklimovv@gmail.com>
2017-12-02 20:58:47 +0200

    DZ#2

---
+++ module-2/task/script.js
@@ -0,0 +1,31 @@
+let participants = prompt('Введите количество путешественников', 'число');
+
+let sharm = 15;
+let hurgada = 25;
+let taba = 6;
+
+
+
+ if (participants <= 0) {
+    alert("Не верное значение!");
+ } else if (parseInt(participants) != participants) {  // или вариант (Math.ceil(participants) - participants > 0)
+    alert("Не целое число!");
+ } else if (participants <= taba){
+    alert(`Можем предложить тур в Табу`);
+    var tabaTrvl = (taba - participants);
+    alert("Осталось " + tabaTrvl + " мест в Табу");
+ } else if (participants > taba && participants <= sharm) {
+     alert('Можем предложить тур в Шармальшейх');
+     var sharmTrvl = (sharm - participants);
+     alert("Осталось " + sharmTrvl + " мест в Шармальшейх");
+ } else if (participants > sharm && participants <= hurgada){
+    alert('Можем предложить тур в Хургаду');
+    var hurTrvl = (hurgada - participants);
+    alert("Осталось " + hurTrvl + " мест в Хургаду");
+ } else {
+     alert("Мест нет!");
+ };
+
+
+
+
